5bb45fd270b9583d0b9f5d101fbbcd8d8daec6fd
Paolo Contessi <paolo.contessi@gmail.com>
2025-09-07 20:27:05 +0200

    chore: add animation

---
+++ app/lib/ConstructionAnimation.tsx
@@ -0,0 +1,9 @@
+import "./construction.css";
+
+export default function () {
+  return (
+    <div className="flex flex-row justify-center m-10">
+        <div className="loader"></div>
+    </div>
+  );
+}
