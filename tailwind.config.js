530ad63b6ff1565431cd026bf8d98f4b73d799e6
Paolo Contessi <paolo.contessi@gmail.com>
2025-09-07 16:41:43 +0200

    chore: honor light/dark mode (#13)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'media', // Use system preference for dark mode
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './app.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
