84dc06b6138e88661f8762259277d8b8476fb39b
Paolo Contessi <paolo.contessi@gmail.com>
2025-12-07 17:13:06 +0100

    fix: migrate from Remix to React Router and update configurations

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from "react-router";
 import { Analytics } from "@vercel/analytics/react";
 
 import "./app.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
+import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import { vercelPreset } from "@vercel/remix/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-
-installGlobals();
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    remix({ presets: [vercelPreset()] }),
+    reactRouter(),
     tsconfigPaths(),
   ],
 });
